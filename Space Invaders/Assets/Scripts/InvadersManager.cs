7733b2e7ed748538fdc0e2d34bcc191b5b3cd497
JulianTeilleux <julian.teilleux@gmail.com>
2014-11-03 15:00:29 +0100

    Test commit

---
+++ Space Invaders/Assets/Scripts/InvadersManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvadersManager : MonoBehaviour
+{
+	public Transform invader1Prefab;
+	public Transform invader2Prefab;
+	public Transform invader3Prefab;
+
+	public Transform activeInvader;
+
+	private bool goingRight;
+
+	public Transform[] invaders;
+	// Use this for initialization
+	void Start ()
+	{
+		invaders = new Transform[55];
+		int index = 0;
+
+		for (int x = 0; x < 11; x++) {
+			for (int y = 1 ; y < 6; y++) {
+
+				if ( y < 2 ) {
+					activeInvader = invader3Prefab;
+				}
+				else if ((y > 1) && (y < 4)) {
+					activeInvader = invader2Prefab;
+				}
+				else {
+					activeInvader = invader1Prefab;
+				}
+
+				invaders[index] = Instantiate(activeInvader) as Transform;
+				invaders[index].transform.position = new Vector3(((x * 4f) - 20), ((y * 4f) - 10), 1);
+
+				invaders[index].transform.parent = transform;
+
+				index++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < invaders.Length; i++) {
+
+			int x = 1;
+
+			if(goingRight == true) {
+				x = 1;
+			}
+
+			if(goingRight == false) {
+				x = -1;
+			}
+
+			if (invaders[i] != null) {
+				invaders[i].transform.Translate(4 * x * Time.deltaTime,0 ,0);
+			}
+		}
+	}
+
+	public void moveInvadersCloser (bool right)
+	{
+		goingRight = !right;
+		Debug.Log (goingRight);
+
+		for (int i = 0; i < invaders.Length; i++) {
+			if (invaders[i] != null) {
+				invaders[i].transform.Translate(0, -1, 0);
+			}		
+		}
+
+	}
+
+	public void destroyInvader (Transform invader)
+	{
+		for (int i = 0; i < invaders.Length; i++)
+		{
+			if (invaders[i] == invader)
+			{
+				Destroy(invader.gameObject);
+				invaders[i] = null;
+			}
+		}
+	}
+}
---
+++ Space Invaders/Assets/Scripts/Ship.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+	/// <summary>
+	/// The bullet prefab.
+	/// </summary>
+	public Transform bulletPrefab;
+
+	private GUIText livesLabel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		livesLabel = GameObject.Find("LivesLabel").guiText;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Left movement
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(-16 * Time.deltaTime, 0, 0);
+		}
+		// Right movement
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(16 * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			shoot ();
+			Debug.Log("pew pew you're dead !");
+		}
+	}
+
+	private void shoot()
+	{
+		Transform Bullet = Instantiate(bulletPrefab) as Transform;
+		Bullet.rigidbody2D.velocity = new Vector2 (0, 30);
+		Bullet.transform.position = new Vector2 (this.transform.position.x, this.transform.position.y);
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Invader"))
+		{
+			Destroy(collision.collider.gameObject);
+			transform.position = new Vector3(0, transform.position.y, 0);
+		}
+	}
+}
